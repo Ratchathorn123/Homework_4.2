6d6aca6d2d24a6b66d9a3af2f7ae507c6da97546
Ratchathorn123 <79511320+Ratchathorn123@users.noreply.github.com>
2021-04-29 22:35:13 +0700

    Add files via upload

---
+++ Homework_4.2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Homework_4._2
+{
+    class Program
+    {
+        static string ReplicateSequence(string halfDNASequence)
+        {
+            string result = "";
+            foreach (char nucleotide in halfDNASequence)
+            {
+                result += "TAGC"["ATCG".IndexOf(nucleotide)];
+            }
+            return result;
+        }
+        int Y;
+        int N;
+        int L;
+
+        int Y;
+        Console.WriteLine("Do you want to process another sequence ? (Y/N) : ");
+
+        int N;
+        Console.Writeline("Please input Y or N.");
+
+        int L;
+        Console.Writeline("Do you want to process another srquence ? (Y/N) : ");
+
+        Console.Readline();
+
+    }
+}
